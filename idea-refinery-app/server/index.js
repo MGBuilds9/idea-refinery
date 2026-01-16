34ccd8eb3be65697b8a9913a12e0470421abcc57
mkgbuilds <michaelguirguis9@gmail.com>
2026-01-16 15:59:41 -0500

    Configure Docker for local IP access and update iOS project files

--- idea-refinery-app/server/index.js
+++ idea-refinery-app/server/index.js
@@ -64,7 +64,10 @@ app.use(cors({
   origin: function (origin, callback) {
     // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1 || allowedOrigins.some(allowed => origin.startsWith(allowed))) {
+    if (allowedOrigins.indexOf(origin) !== -1 || 
+        allowedOrigins.some(allowed => origin.startsWith(allowed)) ||
+        /^https?:\/\/(192\.168\.|10\.|172\.(1[6-9]|2[0-9]|3[0-1])\.)/.test(origin) // Allow local IPs
+       ) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
